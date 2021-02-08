8db643be838734498189a10657625e85c0aee788
llaumegui <guillaumelef59@gmail.com>
2021-02-08 14:53:20 +0100

    Revert "Merge branch 'CleanMargot' into Proto"
    This reverts commit 27e3584723f0f7a282bfb5fded2bb65e71eaee4a, reversing
    changes made to 99be7dbc6d6c1b85f0eec9e9ade0722f3a7714ce.

---
+++ VRSanté/Assets/Resources/_Script/_Controller/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+	public float MouseSensitivity = 100;
+
+	public Transform Player;
+
+	float _xRotation = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Rotation();
+	}
+
+	void Rotation()
+	{
+		float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+		_xRotation -= mouseY;
+		_xRotation = Mathf.Clamp(_xRotation, -90, 75);
+
+		Player.Rotate(Vector3.up * mouseX);
+		transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+
+	}
+}
